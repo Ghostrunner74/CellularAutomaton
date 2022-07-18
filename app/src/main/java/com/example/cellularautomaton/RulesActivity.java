f90f670fe7c8348c5fe349859ff029f9d402ac20
Daniel <jabodav.daniel@gmail.com>
2022-07-18 16:46:12 +0300

    Initialized rules

--- app/src/main/java/com/example/cellularautomaton/RulesActivity.java
+++ app/src/main/java/com/example/cellularautomaton/RulesActivity.java
@@ -1,6 +1,8 @@
 package com.example.cellularautomaton;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -10,20 +12,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class RulesActivity extends AppCompatActivity {
 
+    private SharedPreferences def_pref;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Rules");
         setContentView(R.layout.activity_rules);
-    }
 
-
-    public void newRule(View v) {
+        def_pref = PreferenceManager.getDefaultSharedPreferences(this);
         LinearLayout rulebook = findViewById(R.id.ruleBook);
-        Button[] btn = new Button[10];
 
-        btn[1] = new Button(this);
-        rulebook.addView(btn[1]);
+        int numberOfRules =  Integer.valueOf(def_pref.getString("edit_numberofrules","5"));
+        Button[] btn = new Button[numberOfRules];
+
+        for (int i = 0; i < numberOfRules; i++) {
+            btn[i] = new Button(this);
+            btn[i].setText("Rule " + (i+1));
+            rulebook.addView(btn[i]);
+        }
     }
 
 
